cbaba50e1d63a922f529c1458f8b6756f233381c
ShadowBurst <shadowburst@users.noreply.github.com>
2021-03-19 12:03:10 +0100

    Add set elements to message

--- src/interfaces/IMessage.ts
+++ src/interfaces/IMessage.ts
@@ -3,5 +3,6 @@ import IMessageFormatter from './IMessageFormatter';
 
 export default interface IMessage {
   format(formatter: IMessageFormatter): void;
+  setElements(elements: MessageElement[]): void;
   getElements(): MessageElement[];
 }
